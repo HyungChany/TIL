31999364c6759521ddc2ec4a4657f3e1d5165226
조형찬 <bd3945@naver.com>
2023-07-19 17:06:54 +0900

    형찬이의 공부 노트

---
+++ day1/test1.py
@@ -0,0 +1,16 @@
+a, b = int(input())
+if a == 2:
+	if b == 1:
+		print('A')
+	elif b == 3:
+	    print('B')
+elif a == 3:
+	if b == 2:
+		print('A')
+	elif b == 1:
+        print('B')
+else a == 1:
+	if b == 3:
+		print('A')
+	elif b == 2:
+		print('B')	
