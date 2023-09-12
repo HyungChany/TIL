eb3d9db987e9d7a02aa61886a050ea6e21d559c5
jhc <bd3945@naver.com>
2023-09-12 17:18:26 +0900

    2023,09,12

---
+++ baekjoon/18110.py
@@ -0,0 +1,17 @@
+a=int(input())
+if a==0:
+    print(0)
+else:
+    arr=[int(input()) for _ in range(a)]
+   
+    b=a*0.15
+    if b%1 >= 0.5:
+        b=int(b)+1
+    else: 
+        b=int(b)
+
+    arr=sorted(arr)[b : a-b]
+    
+    if sum(arr)/len(arr)%1 >=0.5:
+        print(sum(arr)//len(arr)+1)
+    else: print(sum(arr)//len(arr))
