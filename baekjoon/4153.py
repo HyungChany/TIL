d74a5554fe9560551589097a0cd0c298c371ef70
jhc <bd3945@naver.com>
2023-09-21 17:51:48 +0900

    2023.09.21

---
+++ baekjoon/4153.py
@@ -0,0 +1,8 @@
+while 1:
+    arr=list(map(int,input().split()))
+    if sum(arr)==0: break
+    arr.sort()
+    
+    if arr[2]**2==arr[0]**2+arr[1]**2:
+        print('right')
+    else: print('wrong')
